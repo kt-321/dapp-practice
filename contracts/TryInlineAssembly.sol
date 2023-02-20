fd94537f5d7859335d4766aaf3bd03916673f79b
kt-321 <kt.shzk247@gmail.com>
2023-02-21 01:20:51 +0900

    add mstore / mload of inline assembly

--- contracts/TryInlineAssembly.sol
+++ contracts/TryInlineAssembly.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.9;
 contract TryInlineAssembly {
     constructor() {}
 
+    event LogUint(uint256 num);
+
     function useStorage2() public returns (uint256 v1, uint256 v2, uint256 v3){
         assembly {
             sstore(0x0, 100)
@@ -43,11 +45,34 @@ contract TryInlineAssembly {
         return res;
     }
 
-    function addition(uint x, uint y) public pure returns (uint256){
+    function addition(uint x, uint y) public returns (uint256){
+        uint res1 = 0;
+        uint res2 = 0;
         assembly {
             let result := add(x, y)
             mstore(0x0, result)
-            return (0x0, 32)
+
+            // return (0x0,32)
+            let empty := mload(0x40) // get free memory pointer
+            mstore(empty, result)
+            res1 := result
+            empty := mload(0x40) // get free memory pointer
+            mstore(empty, 16)
+            res2 := mload(empty)
+            // return (empty, 32) // return 32 bytes from memory address 0x0
+        }
+        emit LogUint(res1);
+        emit LogUint(res2);
+        return res2;
+    }
+
+    function uncheck(
+        uint x,
+        uint y
+    ) public pure returns(uint256){
+        // return x+y;
+        unchecked {
+            return x + y;
         }
     }
 }
